5d3a63f13864f3ea01eaa6ffa8b76f6850908608
Gustavo Herodier <gustavo@tauri-tec.com>
2018-06-19 18:27:16 +0100

    COI button for democrance

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import World from './components/World';
 import Region from './components/Region';
 import Character from './components/Character';
 import Characters from './components/Characters';
+import Coi from './components/Coi';
 import NotFound from './components/NotFound';
 
 const routes = [
@@ -11,6 +12,7 @@ const routes = [
     { path: '/region/:id', component: Region },
     { path: '/characters', component: Characters },
     { path: '/character/:id', component: Character },
+    { path: '/coi', component: Coi },
     { path: '*', component: NotFound }
 ];
 
